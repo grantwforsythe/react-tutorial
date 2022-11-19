d2a8b3493d1f9f1bc07b55b517d71577dd7306c1
Grant Forsythe <grantwforsythe@gmail.com>
2022-11-19 13:06:43 -0500

    Updates

---
+++ precache-manifest.d14b5d73ffd8fe5e9743b0d1f2514806.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3dce595703c95ba765da",
+    "url": "/react-tutorial/static/css/main.8f959574.chunk.css"
+  },
+  {
+    "revision": "3dce595703c95ba765da",
+    "url": "/react-tutorial/static/js/main.e9ed8488.chunk.js"
+  },
+  {
+    "revision": "2a4d04de927634e03e5b",
+    "url": "/react-tutorial/static/js/runtime~main.782a6e4d.js"
+  },
+  {
+    "revision": "2c3aceb9c51772d132ba",
+    "url": "/react-tutorial/static/js/2.9df1e323.chunk.js"
+  },
+  {
+    "revision": "1bbe56eb43d7c17b9907e60ee918b7a8",
+    "url": "/react-tutorial/index.html"
+  }
+];
